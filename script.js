b29543503e70cbe36d39726338a54bcfffa7f409
kiyingijessy <127750795+kiyingijessy@users.noreply.github.com>
2023-09-06 21:44:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const library=[
+    {
+        title:"The Road Ahead",
+        auther:"Bill Gates",
+        status:{
+            own: true,
+            raeding: false,
+            read: false,
+        }
+    },
+    {
+        title:"Steve Jobs",
+        auther:"Walter Isaacson",
+        status:{
+            own: true,
+            raeding: false,
+            read: false,
+        }
+    },
+    {
+        title:"Mockingjay",
+        auther:"Suzanne Collins",
+        status:{
+            own: true,
+            raeding: false,
+            read: false,
+        }
+    },
+]
+const { title: FirstBook} = library[0];
+console.log(FirstBook);
+
